42cacb385e9c03bc59ec1df3e04901e6a472d85e
Lucien Le Roux <rshn.kabu@gmail.com>
2022-05-02 19:55:14 +0200

    working version with transitions

--- src/Globe.jsx
+++ src/Globe.jsx
@@ -1,29 +1,33 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { animated } from 'react-spring';
 import { geoOrthographic, geoPath } from 'd3-geo';
+import useResizeObserver from 'use-resize-observer';
+import colors from 'tailwindcss/colors';
 
-export const Globe = animated(({ features, lat, lng, scale }) => {
-    const ref = useRef();
-    const size = 500;
+export const Globe = animated(({ features, lat, lng, className }) => {
+    const { ref, width = 0, height = 0 } = useResizeObserver();
 
-    const projection = geoOrthographic()
-        .translate([size / 2, size / 2])
-        .scale(scale * size / 2)
-        .clipAngle(90)
-        .rotate([-lng, -lat]);
-    const path = geoPath(projection);
+    const path = geoPath(
+        geoOrthographic()
+            .translate([width / 2, height / 2])
+            .scale(width / 2)
+            .rotate([-lng, -lat])
+            // .fitExtent([[20, 20], [width - 20, height - 20]], country)
+    );
 
     return (
-        <svg ref={ref} width={size} height={size}>
-            <circle cx={size / 2} cy={size / 2} r={size / 2} fill="#171717" />
-            {features.map(({ feature, fill, stroke }, n) => (
-                <path
-                    key={n}
-                    d={path(feature)}
-                    fill={fill}
-                    stroke={stroke}
-                />
-            ))}
-        </svg>
+        <div className={className} ref={ref}>
+            <svg width={width} height={height}>
+                <circle cx={width / 2} cy={height / 2} r={width / 2} fill={colors.neutral[900]} />
+                {features.map(({ feature, fill, stroke }, n) => (
+                    <path
+                        key={n}
+                        d={path(feature)}
+                        fill={fill}
+                        stroke={stroke}
+                    />
+                ))}
+            </svg>
+        </div>
     );
 });
--- src/index.jsx
+++ src/index.jsx
@@ -2,60 +2,71 @@ import React, { useMemo, useState } from 'react';
 import ReactDOM from 'react-dom';
 import { useSpring } from 'react-spring';
 import { geoCentroid, geoGraticule10 } from 'd3-geo';
- 
-import { Globe } from './Globe';
+import colors from 'tailwindcss/colors';
 
+import { Globe } from './Globe';
 import world from './countries.json';
+import './index.css';
 
 const graticule = geoGraticule10();
-const countries = world.features.filter(({ properties }) => !!properties.capital);
 
 const App = () => {
-    // Start with a random country
-    const [country, setCountry] = useState(() => countries[Math.floor(Math.random() * countries.length)]);
-    const features = useMemo(() => [
-        {
-            feature: graticule,
-            fill: 'none',
-            stroke: '#404040',
-        },
-        ...world.features.map((feature) => ({
-            feature,
-            fill: feature.properties.iso === country?.properties.iso ? '#d4d4d4' : "#737373",
-            stroke: '#171717',
-        })),
-    ], [country]);
+    const [countries] = useState(() => world.features.filter(({ properties }) => !!properties.capital).sort(() => Math.random() - 0.5));
+    const [index, setIndex] = useState(170);
+    const [reveal, setReveal] = useState([]);
+    const country = countries[index];
     const center = useMemo(() => {
         const [lng, lat] = country ? geoCentroid(country) : [0, 0];
         return { lat, lng };
     }, [country]);
+    const features = useMemo(() => [
+        {
+            feature: graticule,
+            fill: 'none',
+            stroke: colors.neutral[700],
+        },
+        ...world.features.map((feature) => ({
+            feature,
+            fill: feature.properties.iso === country?.properties.iso ? colors.yellow[100] : countries.some(({ properties }) => properties.iso === feature.properties.iso) ? colors.yellow[500] : colors.yellow[700],
+            stroke: colors.neutral[900],
+        })),
+    ], [country]);
     const { lat, lng } = useSpring(center);
 
+    const onReveal = () => {
+        if (reveal.includes('capital')) {
+            setReveal([]);
+            setIndex((previous) => (previous + 1) % countries.length);
+        } else if (reveal.includes('country')) {
+            setReveal(['country', 'capital']);
+        } else {
+            setReveal(['country']);
+        }
+    };
+ 
     return (
-        <div className="h-screen flex flex-col bg-gray-100">
-            <div>
-                <h1>{country?.properties.emoji} {country?.properties.name}</h1>
-                <h2>{country?.properties.capital}</h2>
-                <h3>{country?.properties.continent}</h3>
+        <div className="p-4 h-screen container mx-auto flex flex-col gap-4">
+            <div className="p-4 flex gap-4 items-center shadow rounded bg-yellow-500">
+                <div>
+                    {index + 1} / {countries.length}
+                </div>
+                <div className="flex-1">
+                    <h3>{country?.properties.continent}</h3>
+                    <div>{reveal.includes('country') ? `${country?.properties.emoji} ${country?.properties.name}` : '???'}</div>
+                    <div>{reveal.includes('capital') ? country?.properties.capital : '???'}</div>
+                </div>
             </div>
-            <Globe features={features} country={country} lat={lat} lng={lng} scale={1}  />
-            <select value={country?.properties.iso} onChange={e => setCountry(countries.find(({ properties }) => properties.iso === e.target.value))}>
-                {Object.entries(
-                    countries.reduce((acc, country) => {
-                        acc[country.properties.continent] = (acc[country.properties.continent] || []);
-                        acc[country.properties.continent].push(country);
-                        return acc;
-                    }, {})
-                ).map(([continent, countries]) => (
-                    <optgroup key={continent} label={continent}>
-                        {countries.map(({ properties }) => (
-                            <option key={properties.iso} value={properties.iso}>
-                                {properties.name}
-                            </option>
-                        ))}
-                    </optgroup>
-                ))}
-            </select>
+            <div className="h-1/2">
+                <Globe
+                    className="m-auto w-full max-h-full aspect-square"
+                    features={features}
+                    lat={lat}
+                    lng={lng}
+                />
+            </div>
+            <button className="p-2 rounded bg-yellow-500 text-white" onClick={onReveal}>
+                NEXT
+            </button>
         </div>
     );
 };
